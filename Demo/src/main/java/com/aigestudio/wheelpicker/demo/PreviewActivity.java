403248c50911215293e1099d0d706cd734a90168
AigeStudio <aigestudio@qq.com>
2016-07-12 11:25:54 +0800

    finish demo

--- Demo/src/main/java/com/aigestudio/wheelpicker/demo/PreviewActivity.java
+++ Demo/src/main/java/com/aigestudio/wheelpicker/demo/PreviewActivity.java
@@ -16,28 +16,28 @@ public class PreviewActivity extends Activity implements WheelPicker.OnItemSelec
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_preview);
 
-//        WheelPicker wheelLeft = (WheelPicker) findViewById(R.id.main_wheel_left);
-//        wheelLeft.setOnItemSelectedListener(this);
-//        WheelPicker wheelCenter = (WheelPicker) findViewById(R.id.main_wheel_center);
-//        wheelCenter.setOnItemSelectedListener(this);
-//        WheelPicker wheelRight = (WheelPicker) findViewById(R.id.main_wheel_right);
-//        wheelRight.setOnItemSelectedListener(this);
+        WheelPicker wheelLeft = (WheelPicker) findViewById(R.id.main_wheel_left);
+        wheelLeft.setOnItemSelectedListener(this);
+        WheelPicker wheelCenter = (WheelPicker) findViewById(R.id.main_wheel_center);
+        wheelCenter.setOnItemSelectedListener(this);
+        WheelPicker wheelRight = (WheelPicker) findViewById(R.id.main_wheel_right);
+        wheelRight.setOnItemSelectedListener(this);
     }
 
     @Override
     public void onItemSelected(WheelPicker picker, Object data, int position) {
-//        String text = "";
-//        switch (picker.getId()) {
-//            case R.id.main_wheel_left:
-//                text = "Left:";
-//                break;
-//            case R.id.main_wheel_center:
-//                text = "Center:";
-//                break;
-//            case R.id.main_wheel_right:
-//                text = "Right:";
-//                break;
-//        }
-//        Toast.makeText(this, text + String.valueOf(data), Toast.LENGTH_SHORT).show();
+        String text = "";
+        switch (picker.getId()) {
+            case R.id.main_wheel_left:
+                text = "Left:";
+                break;
+            case R.id.main_wheel_center:
+                text = "Center:";
+                break;
+            case R.id.main_wheel_right:
+                text = "Right:";
+                break;
+        }
+        Toast.makeText(this, text + String.valueOf(data), Toast.LENGTH_SHORT).show();
     }
 }
--- WheelPicker/src/main/java/com/aigestudio/wheelpicker/widgets/IWheelDatePicker.java
+++
@@ -1,9 +0,0 @@
-package com.aigestudio.wheelpicker.widgets;
-
-/**
- * @author AigeStudio
- * @since 2016-07-11
- */
-public interface IWheelDatePicker {
-
-}
