b914fd73bcdbc783ec0f5ba5a030fd5841ae6769
AigeStudio <aigestudio@qq.com>
2016-07-14 14:42:40 +0800

    add WheelMonthPicker

---
+++ WheelPicker/src/main/java/com/aigestudio/wheelpicker/widgets/IWheelMonthPicker.java
@@ -0,0 +1,32 @@
+package com.aigestudio.wheelpicker.widgets;
+
+/**
+ * 月份选择器方法接口
+ * <p/>
+ * Interface of WheelMonthPicker
+ *
+ * @author AigeStudio 2016-07-12
+ * @version 1
+ */
+public interface IWheelMonthPicker {
+    /**
+     * 获取月份选择器初始化时选择的月份
+     *
+     * @return 选择的月份
+     */
+    int getSelectedMonth();
+
+    /**
+     * 设置月份选择器初始化时选择的月份
+     *
+     * @param month 选择的月份
+     */
+    void setSelectedMonth(int month);
+
+    /**
+     * 获取当前选择的月份
+     *
+     * @return 当前选择的月份
+     */
+    int getCurrentMonth();
+}
---
+++ WheelPicker/src/main/java/com/aigestudio/wheelpicker/widgets/WheelMonthPicker.java
@@ -0,0 +1,55 @@
+package com.aigestudio.wheelpicker.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.aigestudio.wheelpicker.WheelPicker;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class WheelMonthPicker extends WheelPicker implements IWheelMonthPicker {
+    private int mSelectedMonth;
+
+    public WheelMonthPicker(Context context) {
+        this(context, null);
+    }
+
+    public WheelMonthPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        List<Integer> data = new ArrayList<>();
+        for (int i = 1; i <= 12; i++)
+            data.add(i);
+        super.setData(data);
+
+        mSelectedMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
+        updateSelectedYear();
+    }
+
+    private void updateSelectedYear() {
+        setSelectedItemPosition(mSelectedMonth - 1);
+    }
+
+    @Override
+    public void setData(List data) {
+        throw new UnsupportedOperationException("You can not invoke setData in WheelMonthPicker");
+    }
+
+    @Override
+    public int getSelectedMonth() {
+        return mSelectedMonth;
+    }
+
+    @Override
+    public void setSelectedMonth(int month) {
+        mSelectedMonth = month;
+        updateSelectedYear();
+    }
+
+    @Override
+    public int getCurrentMonth() {
+        return Integer.valueOf(String.valueOf(getData().get(getCurrentItemPosition())));
+    }
+}
