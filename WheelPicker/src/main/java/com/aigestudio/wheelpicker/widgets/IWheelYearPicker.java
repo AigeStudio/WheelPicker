b23539135aaf8d202637d7776f7ce0e84f1560f4
AigeStudio <aigestudio@qq.com>
2016-07-14 11:35:54 +0800

    add WheelYearPicker

--- WheelPicker/src/main/java/com/aigestudio/wheelpicker/widgets/IWheelYearPicker.java
+++ WheelPicker/src/main/java/com/aigestudio/wheelpicker/widgets/IWheelYearPicker.java
@@ -1,7 +1,68 @@
 package com.aigestudio.wheelpicker.widgets;
 
 /**
- * Created by Administrator on 2016/7/13.
+ * 年份选择器方法接口
+ * <p>
+ * Interface of WheelYearPicker
+ *
+ * @author AigeStudio 2016-07-12
+ * @version 1
  */
-public class IWheelYearPicker {
+public interface IWheelYearPicker {
+    /**
+     * 设置年份范围
+     *
+     * @param start 开始的年份
+     * @param end   结束的年份
+     */
+    void setYearFrame(int start, int end);
+
+    /**
+     * 获取开始的年份
+     *
+     * @return 开始的年份
+     */
+    int getYearStart();
+
+    /**
+     * 设置开始的年份
+     *
+     * @param start 开始的年份
+     */
+    void setYearStart(int start);
+
+    /**
+     * 获取结束的年份
+     *
+     * @return 结束的年份
+     */
+    int getYearEnd();
+
+    /**
+     * 设置结束的年份
+     *
+     * @param end 结束的年份
+     */
+    void setYearEnd(int end);
+
+    /**
+     * 获取年份选择器初始化时选中的年份
+     *
+     * @return 年份选择器初始化时选中的年份
+     */
+    int getSelectedYear();
+
+    /**
+     * 设置年份选择器初始化时选中的年份
+     *
+     * @param year 年份选择器初始化时选中的年份
+     */
+    void setSelectedYear(int year);
+
+    /**
+     * 获取当前选中的年份
+     *
+     * @return 当前选中的年份
+     */
+    int getCurrentYear();
 }
--- WheelPicker/src/main/java/com/aigestudio/wheelpicker/widgets/WheelYearPicker.java
+++ WheelPicker/src/main/java/com/aigestudio/wheelpicker/widgets/WheelYearPicker.java
@@ -1,7 +1,98 @@
 package com.aigestudio.wheelpicker.widgets;
 
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.aigestudio.wheelpicker.WheelPicker;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
 /**
- * Created by Administrator on 2016/7/13.
+ * 年份选择器
+ * <p>
+ * Picker for Years
+ *
+ * @author AigeStudio 2016-07-12
+ * @version 1
  */
-public class WheelYearPicker {
+public class WheelYearPicker extends WheelPicker implements IWheelYearPicker {
+    private int mYearStart = 1000, mYearEnd = 3000;
+    private int mSelectedYear;
+
+    public WheelYearPicker(Context context) {
+        this(context, null);
+    }
+
+    public WheelYearPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        updateYears();
+        mSelectedYear = Calendar.getInstance().get(Calendar.YEAR);
+        updateSelectedYear();
+    }
+
+    private void updateYears() {
+        List<Integer> data = new ArrayList<>();
+        for (int i = mYearStart; i <= mYearEnd; i++)
+            data.add(i);
+        super.setData(data);
+    }
+
+    private void updateSelectedYear() {
+        setSelectedItemPosition(mSelectedYear - mYearStart);
+    }
+
+    @Override
+    public void setData(List data) {
+        throw new UnsupportedOperationException("You can not invoke setData in WheelYearPicker");
+    }
+
+    @Override
+    public void setYearFrame(int start, int end) {
+        mYearStart = start;
+        mYearEnd = end;
+        updateYears();
+        updateSelectedYear();
+    }
+
+    @Override
+    public int getYearStart() {
+        return mYearStart;
+    }
+
+    @Override
+    public void setYearStart(int start) {
+        mYearStart = start;
+        updateYears();
+        updateSelectedYear();
+    }
+
+    @Override
+    public int getYearEnd() {
+        return mYearEnd;
+    }
+
+    @Override
+    public void setYearEnd(int end) {
+        mYearEnd = end;
+        updateYears();
+    }
+
+    @Override
+    public int getSelectedYear() {
+        return mSelectedYear;
+    }
+
+    @Override
+    public void setSelectedYear(int year) {
+        mSelectedYear = year;
+        updateSelectedYear();
+    }
+
+    @Override
+    public int getCurrentYear() {
+        return Integer.valueOf(String.valueOf(getData().get(getCurrentItemPosition())));
+    }
 }
