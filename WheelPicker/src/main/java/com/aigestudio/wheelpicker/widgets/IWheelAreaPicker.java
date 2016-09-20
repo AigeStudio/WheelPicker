9a7defde71ff49f7488a3f567dac06e4e0a90814
goushengli <sheyongcan@gmail.com>
2016-09-20 16:00:00 +0800

    WheelAreaPicker

---
+++ WheelPicker/src/main/java/com/aigestudio/wheelpicker/widgets/IWheelAreaPicker.java
@@ -0,0 +1,13 @@
+package com.aigestudio.wheelpicker.widgets;
+
+/**
+ * Created by Administrator on 2016/9/20 0020.
+ */
+
+public interface IWheelAreaPicker {
+    String getProvince();
+
+    String getCity();
+
+    String getArea();
+}
